8cf24a701de1aaadf1c19f93373267e71ba2f821
Lukas Stracke <lukas.stracke@sentry.io>
2024-05-27 14:51:38 +0200

    fix

--- index.js
+++ index.js
@@ -5,23 +5,44 @@ Sentry.init({
   integrations: (integrations) => {
     return integrations.filter((integration) => {
       // Uncomment any of these 3 integrations to see the error
-      if (
-        integration.name === "Http" ||
-        integration.name === "AwsLambda" ||
-        integration.name === "Aws"
-      ) {
-        return false;
-      }
+      // if (
+      //   // integration.name === "Http" ||
+      //   integration.name === "AwsLambda" ||
+      //   integration.name === "Aws"
+      // ) {
+      //   return false;
+      // }
       return true;
     });
+  },
+  tracesSampleRate: 1.0,
+  beforeSendTransaction: (transaction) => {
+    console.log("beforeSendTransaction", transaction);
+    return transaction;
   },
   debug: true,
 });
 
+const http = require("http");
 console.log("hello");
 
 function handle() {
-  console.log("handle");
+  Sentry.startSpanManual({ name: "test", op: "test" }, (span) => {
+    http.get("http://example.com", (res) => {
+      console.log("statusCode:", res.statusCode);
+      console.log("headers:", res.headers);
+
+      res.on("data", (d) => {
+        process.stdout.write(d);
+      });
+
+      res.on("end", () => {
+        span.end();
+      });
+    });
+  });
 }
+
+handle();
 
 module.exports = { handle };
